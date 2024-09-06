1cd882583a7f68c698f034ae6b38563d53ce7eaf
nebula <karlo.tsutskiridze@gmail.com>
2024-09-06 23:20:54 +0400

    added types

---
+++ types/settings.d.ts
@@ -0,0 +1,10 @@
+
+export type EditorSettings = {
+  theme: "Noctis Lilac" | "Dracula" | "Default";
+  lineNumbers: boolean;
+  lineWrapping: boolean;
+  highlightActiveLine: boolean;
+  matchBrackets: boolean;
+  transparentBackground: boolean;
+}
+
